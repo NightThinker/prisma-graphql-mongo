21f009e06f0ba619bcc9e15f53a24bcdbe4561d2
May <may@Gato-Pro.local>
2019-03-21 16:14:54 +0700

    bug filter because prisma server not generate OR

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,4 +1,16 @@
-const feed = (root, args, context, info) => context.prisma.links()
+const feed = async (parent, args, context, info) => {
+  const where = args.filter ? {
+    OR: [
+      { description_contains: args.filter },
+      { url_contains: args.filter },
+    ],
+  } : {}
+
+  const links = await context.prisma.links({
+    where
+  })
+  return links
+}
 
 module.exports = {
   feed
